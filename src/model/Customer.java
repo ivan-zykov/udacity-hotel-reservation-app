b35391969d27fd1ccf8e46c710b39737cc739553
Ivan Zykov <ivan.v.zykov@gmail.com>
2022-04-15 16:51:06 +0200

    Moves email validation in Customer class to a separate method

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -7,16 +7,12 @@ public class Customer implements Comparable<Customer> {
     final private String firstName, lastName, email;
 
     public Customer(String firstName, String lastName, String email) {
-        // Validate email
-        final String emailRegex = "^(.+)@(.+).(.+)$";
-        Pattern emailPattern = Pattern.compile(emailRegex);
-        if (emailPattern.matcher(email).matches()) {
+        if (isValidEmail(email)) {
             this.email = email;
         } else {
             throw new IllegalArgumentException("Email is of wrong format. " +
                     "Please correct your email.");
         }
-
         this.firstName = firstName;
         this.lastName = lastName;
     }
@@ -41,5 +37,11 @@ public class Customer implements Comparable<Customer> {
     @Override
     public int compareTo(Customer customer) {
         return this.email.compareTo(customer.getEmail());
+    }
+
+    private boolean isValidEmail(String email) {
+        final String emailRegex = "^(.+)@(.+).(.+)$";
+        Pattern emailPattern = Pattern.compile(emailRegex);
+        return emailPattern.matcher(email).matches();
     }
 }
