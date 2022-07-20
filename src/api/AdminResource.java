6f839e17510c6201252b5439e73b0d8b4c48f35c
Ivan Zykov <ivan.v.zykov@gmail.com>
2022-07-20 14:11:41 +0200

    Adds JavaDocs for api package

--- src/api/AdminResource.java
+++ src/api/AdminResource.java
@@ -10,6 +10,9 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * API which serves as intermediary between the admin UI and the services.
+ */
 public final class AdminResource {
 
     private static AdminResource INSTANCE;
@@ -23,6 +26,14 @@ public final class AdminResource {
         this.reservationService = reservationService;
     }
 
+    /**
+     * Provides unique instance of this singleton service.
+     *
+     * @param customerService       customerService object that handles {@link model.Customer}s
+     * @param reservationService    reservationService object that handles {@link model.IRoom}s and
+     *                              {@link model.Reservation}s
+     * @return                      adminResource object of this service
+     */
     public static AdminResource getInstance(CustomerService customerService,
                                             ReservationService reservationService) {
         if (INSTANCE == null) {
@@ -32,10 +43,21 @@ public final class AdminResource {
         return INSTANCE;
     }
 
+    /**
+     * Calls a service to get a customer with the supplied email.
+     *
+     * @param email string with email of the required customer
+     * @return      customer object which has the supplied email
+     */
     public Customer getCustomer(String email) {
         return customerService.getCustomer(email);
     }
 
+    /**
+     * Calls a service to record a collection of new rooms.
+     *
+     * @param rooms list of new rooms to record
+     */
     public void addRoom(List<IRoom> rooms) {
 
         for (IRoom newRoom: rooms) {
@@ -43,16 +65,29 @@ public final class AdminResource {
         }
     }
 
+    /**
+     * Calls a service to get all recorded rooms.
+     *
+     * @return collection of all recorded rooms
+     */
     public Collection<IRoom> getAllRooms() {
 
         Map<String, IRoom> allRooms = reservationService.getRooms();
         return new ArrayList<>(allRooms.values());
     }
 
+    /**
+     * Calls a service to get all customers.
+     *
+     * @return  collection of all recorded customers
+     */
     public Collection<Customer> getAllCustomers() {
         return customerService.getAllCustomers();
     }
 
+    /**
+     * Calls a service to print all recorded reservations to the console.
+     */
     public void displayAllReservations() {
         reservationService.printAllReservations();
     }
--- src/api/HotelResource.java
+++ src/api/HotelResource.java
@@ -9,6 +9,9 @@ import service.ReservationService;
 import java.util.Collection;
 import java.util.Date;
 
+/**
+ * API which serves as intermediary between the customer UI and the services.
+ */
 public final class HotelResource {
 
     private static HotelResource INSTANCE;
@@ -23,6 +26,14 @@ public final class HotelResource {
         this.reservationService = reservationService;
     }
 
+    /**
+     * Provides unique instance of this singleton service.
+     *
+     * @param customerService       customerService object that handles {@link model.Customer}s
+     * @param reservationService    reservationService object that handles {@link model.IRoom}s and
+     *                              {@link model.Reservation}s
+     * @return                      hotelResource object of this service
+     */
     public static HotelResource getInstance(CustomerService customerService,
                                             ReservationService reservationService) {
 
@@ -33,18 +44,47 @@ public final class HotelResource {
         return INSTANCE;
     }
 
+    /**
+     * Calls a service to get a customer with the supplied email.
+     *
+     * @param email string with email of the required customer
+     * @return      customer object which has the supplied email
+     */
     public Customer getCustomer(String email) {
         return customerService.getCustomer(email);
     }
 
+    /**
+     * Calls a service to record a new {@link model.Customer} with supplied data.
+     *
+     * @param email     string with customer's email
+     * @param firstName string with customer's first name
+     * @param lasName   string with customer's last name
+     */
     public void createACustomer(String email, String firstName, String lasName) {
         customerService.addCustomer(email, firstName, lasName);
     }
 
+    /**
+     * Calls a service to get a room with the provided room number.
+     *
+     * @param roomNumber    string with the number of the required room
+     * @return              iRoom object of the room with the supplied number
+     */
     public IRoom getRoom(String roomNumber) {
         return reservationService.getARoom(roomNumber);
     }
 
+    /**
+     * Calls a service to get a {@link model.Customer} with the supplied email and reserve a room for the acquired
+     * customer.
+     *
+     * @param customerEmail string with the email of the customer booking a room
+     * @param room          iRoom object of the room to book
+     * @param checkInDate   date object of check-in
+     * @param checkOutDate  date object of check-out
+     * @return              reservation object as the result of booking
+     */
     public Reservation bookARoom(String customerEmail, IRoom room, Date checkInDate,
                                  Date checkOutDate) {
 
@@ -53,12 +93,25 @@ public final class HotelResource {
                 checkOutDate);
     }
 
+    /**
+     * Calls a service to get a {@link model.Customer} with the supplied email and get all their reservations.
+     *
+     * @param customerEmail string with the email of the customer booking a room
+     * @return              collection of reservations recorded for the customer with supplied email
+     */
     public Collection<Reservation> getCustomersReservations(String customerEmail) {
 
         Customer customer = getCustomer(customerEmail);
         return reservationService.getCustomersReservation(customer);
     }
 
+    /**
+     * Calls a service to find rooms available for booking for the supplied dates.
+     *
+     * @param checkIn   date object of check-in
+     * @param checkOut  date object of check-out
+     * @return          collection of rooms available for booking
+     */
     public Collection<IRoom> findARoom(Date checkIn, Date checkOut) {
         return reservationService.findRooms(checkIn, checkOut);
     }
